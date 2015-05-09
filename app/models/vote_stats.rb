54821f16abbf828d8e66a0409897985779a57ddf
Daniel Ferraz <d.ferrazm@gmail.com>
2015-05-09 14:01:12 -0300

    Remove refresh_percentage method

--- app/models/vote_stats.rb
+++ app/models/vote_stats.rb
@@ -8,15 +8,11 @@ class VoteStats
   end
 
   def self.percentage
-     refresh_percentage
-  end
-
-  def self.refresh_percentage
     contestant_votes = votes_per_contestant
     total = contestant_votes.values.sum
     greater = contestant_votes.first[0] # first key
     contestant_votes.each_key do |cont|
-      contestant_votes[cont] = ((contestant_votes[cont] / (total.zero? ? 1 : total).to_d) * 100).round.to_i 
+      contestant_votes[cont] = ((contestant_votes[cont] / (total.zero? ? 1 : total).to_d) * 100).round.to_i
       greater = cont if contestant_votes[cont] > contestant_votes[greater]
     end
     { percentages: contestant_votes, greater: greater }.to_json
--- spec/models/vote_stats_spec.rb
+++ spec/models/vote_stats_spec.rb
@@ -2,32 +2,25 @@ require 'rails_helper'
 
 describe VoteStats do
   describe 'methods' do
-    describe 'refresh_percentage' do
+    describe 'percentage' do
       before do
         clear_redis
       end
 
       it 'returns 0 for each contestant when there are no votes' do
-        expect(VoteStats.refresh_percentage).to eq({ percentages: { '1' => 0, '2' => 0 }, greater: '1'}.to_json)
+        expect(VoteStats.percentage).to eq({ percentages: { '1' => 0, '2' => 0 }, greater: '1'}.to_json)
       end
 
       it 'returns the percentage of each contestant based on the cached counters' do
         allow(ContestantStore).to receive(:votes).with('1') { 1 } # 25 %
         allow(ContestantStore).to receive(:votes).with('2') { 3 } # 75 %
-        expect(VoteStats.refresh_percentage).to eq({ percentages: { '1' => 25, '2' => 75 }, greater: '2'}.to_json)
+        expect(VoteStats.percentage).to eq({ percentages: { '1' => 25, '2' => 75 }, greater: '2'}.to_json)
       end
 
       it 'rounds up the percentage and returns integer numbers' do
         allow(ContestantStore).to receive(:votes).with('1') { 2 } # 29 %
         allow(ContestantStore).to receive(:votes).with('2') { 5 } # 71 %
-        expect(VoteStats.refresh_percentage).to eq({ percentages: { '1' => 29, '2' => 71 }, greater: '2'}.to_json)
-      end
-    end
-
-    describe 'percentage' do
-      it 'refreshes the percentage' do
-        allow(VoteStats).to receive(:refresh_percentage) { 'refreshed' }
-        expect(VoteStats.percentage).to eq 'refreshed'
+        expect(VoteStats.percentage).to eq({ percentages: { '1' => 29, '2' => 71 }, greater: '2'}.to_json)
       end
     end
 
